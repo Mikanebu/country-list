b3593b758fede3c7970314497473225537ea1821
Dmytro Gierman <mryakzp@gmail.com>
2017-12-11 20:55:21 +0100

    methods equal to original npm 'country-list' is created.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-let countryList = require('./data.json')
+let countryList = require('./data.json');
 
-console.log(countryList)
+// prepare data structures
+let
+    codeToName = new Object(),
+    nameToCode = new Object(),
+    names = new Array(),
+    codes = new Array();
+
+countryList.map( obj => {
+    codeToName[obj.Code] = obj.Name;
+    nameToCode[obj.Name] = obj.Code;
+
+    names.push(obj.Name);
+    codes.push(obj.Code);
+});
+
+module.exports = {
+    getName : (code) => codeToName[code],
+    getCode : (name) => nameToCode[name],
+    getNames : () => names,
+    getCodes : () => codes,
+    getNameList : () => nameToCode,
+    getCodeList : () => codeToName
+};
